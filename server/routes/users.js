53aed9eda89a54f8c381d16bec1b8524ec2a037e
keni21 <david.erlacher@outlook.com>
2018-04-07 13:05:02 +0200

    delete hinzugefuegt (User)

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,6 +5,8 @@ const db = require("../database");
 
 router.get("/", getAllUsers);
 router.get("/:ID", singleUser);
+router.post("/",insertUser);
+router.delete("/:ID",deleteUser);
 
 let userCollection = db.getCollection("users");
 
@@ -16,6 +18,20 @@ function getAllUsers(req, res)
 function singleUser (req, res)
 {
   res.json(userCollection.get(req.params.ID));
+}
+
+function insertUser (req, res)
+{
+  let username = new User(req.body.username);
+  userCollection.insert(username);
+  res.json(username);
+}
+
+function deleteUser(req, res)
+{ 
+  let user = userCollection.get(req.params.ID)
+  userCollection.remove(user);
+  res.json(user);
 }
 
 module.exports = router;
